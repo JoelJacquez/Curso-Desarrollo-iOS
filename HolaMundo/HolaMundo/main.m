313c0e653971a54d6183386e962f93931524e549
Joel Jacquez <cryptt3r@gmail.com>
2012-11-23 15:34:20 -0700

    Hola Mundo

---
+++ HolaMundo/HolaMundo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HolaMundo
+//
+//  Created by Joel Jacquez @cryptt3r on 21/11/12.
+//  Copyright (c) 2012 tecnobits.net. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
